4207b37a3d3e9803fda8d3b7db1943cff7cfac4d
iMeeTake <arceniy_radaev@mail.ru>
2025-05-22 13:16:29 +0500

    1.0.0

---
+++ Effectual 1.21.5/src/client/java/com/imeetake/effectual/EffectualClient.java
@@ -0,0 +1,10 @@
+package com.imeetake.effectual;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EffectualClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
